28a934f52b76c855b43294253ac0bbf6c19be3c9
Henrik Rodrigues Marinho <henrikrodriguesjob@gmail.com>
2022-09-07 15:22:06 -0300

    Class
    i redid the project using classes

--- script.js
+++ script.js
@@ -1,213 +1,140 @@
 const listOp = document.getElementById("listFigures");
-const area = document.getElementById("area");
-const result = document.getElementById("result");
-
-function square() {
-    area.textContent = "";
-    result.textContent = "";
-    const input = document.createElement("input");
-    input.type = "number";
-    const label = document.createElement("label");
-    label.textContent = "Side: ";
-    area.appendChild(label)
-    label.appendChild(input);
-
-    const btn = document.createElement("button");
-    area.appendChild(btn);
-    btn.textContent = "Check"
-    btn.addEventListener("click" , () => {
-        const finalValue = input.value**2;
-        result.innerHTML = `<p> The area of the square is: ${finalValue.toFixed(2).replace("." , ",")} m^2.`;
-    });
-};
-
-function rectangle() {
-    area.textContent = "";
-    result.textContent = "";
-    const inputb = document.createElement("input");
-    inputb.type = "number";
-    const labelb = document.createElement("label");
-    labelb.textContent = "Base: ";
-    area.appendChild(labelb);
-    labelb.appendChild(inputb);
-
-    const inputh = document.createElement("input");
-    inputh.type = "number";
-    const labelh =  document.createElement("label");
-    labelh.textContent = "Height: ";
-    area.appendChild(labelh);
-    labelh.appendChild(inputh);
-
-    const btn = document.createElement("button");
-    area.appendChild(btn)
-    btn.textContent = "Check";
-    btn.addEventListener("click", () => {
-        const finalValue = inputb.value * inputh.value;
-        result.innerHTML = `<p>The area of the rectangle is: ${finalValue.toFixed(2).replace("." , ",")} m^2.`;
-    });
-};
-
-function rhombus() {
-    area.textContent = "";
-    result.textContent = "";
-    const inputD = document.createElement("input");
-    inputD.type = "number";
-    const labelD = document.createElement("label");
-    labelD.textContent = "Bigger diagonal: ";
-    area.appendChild(labelD);
-    labelD.appendChild(inputD);
-
-    const inputd = document.createElement("input");
-    inputd.type = "number";
-    const labeld = document.createElement("label");
-    labeld.textContent = "Minor diagonal: ";
-    area.appendChild(labeld);
-    labeld.appendChild(inputd);
-
-    const btn = document.createElement("button");
-    area.appendChild(btn);
-    btn.textContent = "Check";
-    btn.addEventListener("click", () => {
-        const finalValue = inputD.value * inputd.value / 2;
-        result.innerHTML = `<p>The area of the rhombus is: ${finalValue.toFixed(2).replace("." , ",")} m^2.`;
-    });
-};
-
-function parallelogram() {
-    area.textContent = "";
-    result.textContent = "";
-    const inputb = document.createElement("input");
-    inputb.type = "number";
-    const labelb = document.createElement("label");
-    labelb.textContent = "Base: ";
-    area.appendChild(labelb);
-    labelb.appendChild(inputb);
-
-    const inputh = document.createElement("input");
-    inputh.type = "number";
-    const labelh = document.createElement("label");
-    labelh.textContent = "Height: ";
-    area.appendChild(labelh);
-    labelh.appendChild(inputh);
-
-    const btn = document.createElement("button");
-    area.appendChild(btn);
-    btn.textContent = "Check";
-    btn.addEventListener("click", () => {
-        const finalValue = inputb.value * inputh.value;
-        result.innerHTML = `<p>The area of the parallelogram is: ${finalValue.toFixed(2).replace("." , ",")} m^2.`;
-    });
-};
-
-function trapezium() {
-    area.textContent = "";
-    result.textContent = "";
-    const inputB = document.createElement("input");
-    inputB.type = "number";
-    const labelB = document.createElement("label");
-    labelB.textContent = "Bigger base: ";
-    area.appendChild(labelB);
-    labelB.appendChild(inputB);
-
-    const inputb = document.createElement("input");
-    inputb.type =  "number";
-    const labelb = document.createElement("label");
-    labelb.textContent = "Smaller base: ";
-    area.appendChild(labelb);
-    labelb.appendChild(inputb);
-
-    const inputh = document.createElement("input");
-    inputh.type = "number";
-    const labelh = document.createElement("label");
-    labelh.textContent = "Height: ";
-    area.appendChild(labelh);
-    labelh.appendChild(inputh);
-
-    const btn = document.createElement("button");
-    area.appendChild(btn);
-    btn.textContent = "Check";
-    btn.addEventListener("click", () => {
-        const finalValue = (Number(inputB.value) + Number(inputb.value)) * inputh.value / 2
-        result.innerHTML = `The area of the trapezium is: ${finalValue.toFixed(2).replace("." , ",")} m^2.`;
-    });
-};
-
-function triangle() {
-    area.textContent = "";
-    result.textContent = "";
-    const inputb = document.createElement("input");
-    inputb.type = "number";
-    const labelb = document.createElement("label");
-    labelb.textContent = "Base: ";
-    area.appendChild(labelb);
-    labelb.appendChild(inputb);
-
-    const inputh = document.createElement("input");
-    inputh.type = "number";
-    const labelh = document.createElement("label");
-    labelh.textContent = "Height: ";
-    area.appendChild(labelh);
-    labelh.appendChild(inputh);
-
-    const btn = document.createElement("button");
-    area.appendChild(btn);
-    btn.textContent = "Check";
-    btn.addEventListener("click", () => {
-        const finalValue = inputb.value * inputh.value / 2;
-        result.innerHTML = `<p>The area of the triangle is: ${finalValue.toFixed(2).replace("." , ",")} m^2.`;
-    })
-};
-
-function circle() {
-    area.textContent = "";
-    result.textContent = "";
-    const inputPi = document.createElement("input");
-    inputPi.type = "number";
-    inputPi.value = "3.14"
-    const labelPi = document.createElement("label");
-    labelPi.textContent = "π: ";
-    area.appendChild(labelPi);
-    labelPi.appendChild(inputPi);
-
-    const inputr = document.createElement("input");
-    inputr.type = "number";
-    const labelr = document.createElement("label");
-    labelr.textContent = "Radius: ";
-    area.appendChild(labelr);
-    labelr.appendChild(inputr);
-
-    const btn = document.createElement("button");
-    area.appendChild(btn);
-    btn.textContent = "Check";
-    btn.addEventListener("click", () => {
-        const finalValue = (inputr.value**2) * inputPi.value;
-        result.innerHTML = `The area of the circle is: ${finalValue.toFixed(2).replace("." , ",")} m^2.`
-    });
-};
+const footResult = document.getElementById("result");
+const sections = document.querySelectorAll("section");
 
 listOp.addEventListener("change" , () => {
     const figure = listOp.options[listOp.selectedIndex].value;
+    footResult.hidden = true;
+    for(let section of sections){
+        section.hidden = true;
+    };
+    
     switch(figure) {
         case "square":
-            square();
+            document.getElementById("square").hidden = false;
             break;
         case "rectangle":
-            rectangle();
+            document.getElementById("rectangle").hidden = false;
             break;
         case "rhombus":
-            rhombus();
+            document.getElementById("rhombus").hidden = false;
             break;
         case "parallelogram":
-            parallelogram();
+            document.getElementById("parallelogram").hidden = false;
             break;
         case "trapezium":
-            trapezium();
+            document.getElementById("trapezium").hidden = false;
             break;
         case "triangle":
-            triangle();
+            document.getElementById("triangle").hidden = false;
             break;
         case "circle":
-            circle();
+            document.getElementById("circle").hidden = false;
             break;
     };
+});
+class Square{
+    constructor(){
+        this.side = document.getElementById("sideSquare").valueAsNumber;
+    }
+    calcArea(){
+        const result = this.side ** 2;
+        footResult.hidden = false;
+        footResult.innerHTML = `<p>The area of the square is ${result.toFixed(2).replace("." , ",")} m<sup>2</sup>.</p>`
+    }
+};
+document.getElementById("btnSquare").addEventListener("click" , () => {
+    const square = new Square();
+    square.calcArea();
+});
+class Rectangle{
+    constructor(){
+        this.base = document.getElementById("baseRectangle").valueAsNumber;
+        this.height = document.getElementById("heightRectangle").valueAsNumber;
+    }
+    calcArea(){
+        const result = this.base * this.height;
+        footResult.hidden = false;
+        footResult.innerHTML = `<p>The area of the rectangle is ${result.toFixed(2).replace("." , ",")} m<sup>2</sup>.</p>`
+    }
+};
+document.getElementById("btnRectangle").addEventListener("click", () => {
+    const rectangle = new Rectangle();
+    rectangle.calcArea();
+});
+class Rhombus{
+    constructor(){
+        this.biggerDiagonal = document.getElementById("biggerDiagonalRhombus").valueAsNumber;
+        this.smallerDiagonal = document.getElementById("smallerDiagonalRhombus").valueAsNumber;
+    }
+    calcArea(){
+        const result = this.biggerDiagonal * this.smallerDiagonal / 2;
+        footResult.hidden = false;
+        footResult.innerHTML = `<p>The area of the rhombus is ${result.toFixed(2).replace("." , ",")} m<sup>2</sup>.</p>`
+    }
+}
+document.getElementById("btnRhombus").addEventListener("click", () => {
+    const rhombus = new Rhombus();
+    rhombus.calcArea();
+});
+class Parallelogram{
+    constructor(){
+        this.base = document.getElementById("baseParallelogram").valueAsNumber;
+        this.height = document.getElementById("heightParallelogram").valueAsNumber;
+    }
+    calcArea(){
+        const result = this.base * this.height;
+        footResult.hidden = false;
+        footResult.innerHTML = `<p>The area of the parallelogram is ${result.toFixed(2).replace("." , ",")} m<sup>2</sup>.</p>`
+    }
+};
+document.getElementById("btnParallelogram").addEventListener("click" , () => {
+    const parallelogram = new Parallelogram();
+    parallelogram.calcArea();
+});
+class Trapezium{
+    constructor(){
+        this.biggerBase = document.getElementById("biggerBaseTrapezium").valueAsNumber;
+        this.smallerBase = document.getElementById("smallerBaseTrapezium").valueAsNumber;
+        this.height = document.getElementById("heightTrapezium").valueAsNumber;
+    }
+    calcArea(){
+        const result = (this.biggerBase + this.smallerBase) * this.height / 2;
+        footResult.hidden = false;
+        footResult.innerHTML = `<p>The area of the trapezium is ${result.toFixed(2).replace("." , ",")} m<sup>2</sup>.</p>`
+    }
+};
+document.getElementById("btnTrapezium").addEventListener("click", () => {
+    const trapezium = new Trapezium();
+    trapezium.calcArea();
+});
+class Triangle{
+    constructor(){
+        this.base = document.getElementById("baseTriangle").valueAsNumber;
+        this.height = document.getElementById("heightTriangle").valueAsNumber;
+    }
+    calcArea(){
+        const result = this.base * this.height / 2;
+        footResult.hidden = false;
+        footResult.innerHTML = `<p>The area of the triangle is ${result.toFixed(2).replace("." , ",")} m<sup>2</sup>.</p>`
+    }
+};
+document.getElementById("btnTriangle").addEventListener("click" , () => {
+    const triangle = new Triangle();
+    triangle.calcArea();
+});
+class Circle{
+    constructor(){
+        this.pi = document.getElementById("piCircle").valueAsNumber;
+        this.radius = document.getElementById("radiusCircle").valueAsNumber;
+    }
+    calcArea(){
+        const result = this.pi * (this.radius ** 2);
+        footResult.hidden = false;
+        footResult.innerHTML = `<p>The area of the circle is ${result.toFixed(2).replace("." , ",")} m<sup>2</sup>.</p>`
+    }
+};
+document.getElementById("btnCircle").addEventListener("click" , () => {
+    const circle = new Circle();
+    circle.calcArea();
 });
